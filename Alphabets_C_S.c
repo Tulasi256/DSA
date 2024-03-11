d266ba6c4c6790c92bbb4bddd0c6dc4d51895b6c
Tulasi256 <130477467+Tulasi256@users.noreply.github.com>
2024-03-12 00:47:36 +0530

    Add files via upload

---
+++ Alphabets_C_S.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+
+    for(int i=65;i<91;i++)
+    {
+        printf(" %c",(char)i);
+    }
+   
+     for(int j=97;j<123;j++)
+    {
+        printf(" %c",(char)j);
+    }
+
+}
